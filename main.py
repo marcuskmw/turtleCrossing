3f3f9b521722b191ad555874f34e0ad8de61cbdc
Marcus Wong <marcuswongkaiman@gmail.com>
2021-11-06 13:19:10 +0800

    setup player

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+player = Player()
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+screen.onkey(key="Up", fun=player.move_up)
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+
--- player.py
+++ player.py
@@ -1,7 +1,23 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.shape("turtle")
+        self.setheading(90)
+        self.color("green")
+        self.goto(STARTING_POSITION)
+        self.showturtle()
+
+    def move_up(self):
+        if self.ycor() < FINISH_LINE_Y:
+            self.goto(self.xcor(), self.ycor() + MOVE_DISTANCE)
+        else:
+            self.goto(STARTING_POSITION)
